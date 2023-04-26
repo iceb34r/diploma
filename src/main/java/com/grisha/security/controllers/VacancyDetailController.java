bf0df02d7cdd1c7e946e1cc44b7ba7c8bea099e4
iceb34r <currysss@mail.ru>
2023-04-26 17:17:06 +0300

    +vacancy response

--- src/main/java/com/grisha/security/controllers/VacancyDetailController.java
+++ src/main/java/com/grisha/security/controllers/VacancyDetailController.java
@@ -6,17 +6,16 @@ import com.grisha.security.entities.User;
 import com.grisha.security.entities.Vacancy;
 import com.grisha.security.repositories.EmployerRepository;
 import com.grisha.security.repositories.VacancyRepository;
+import com.grisha.security.services.EmailSenderService;
 import com.grisha.security.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import java.security.Principal;
 
 @Controller
@@ -25,15 +24,24 @@ public class VacancyDetailController {
     @Autowired
     private UserService userService;
     @Autowired
-    private EmployerRepository employerRepository;
-    @Autowired
     private VacancyRepository vacancyRepository;
+    @Autowired
+    private EmailSenderService emailSenderService;
     @GetMapping
-    public String vacancyDetails(@PathVariable("id") Long id, Model model, Principal principal) {
+    public String vacancyDetails(@PathVariable("id") Long id, Model model, Principal principal) throws MessagingException {
         Vacancy vacancy = vacancyRepository.findVacancyById(id);
         Employer employer = vacancy.getEmployer();
         model.addAttribute("vacancy", vacancy);
         model.addAttribute("employer", employer);
         return "vacancydetails";
+    }
+    @PostMapping
+    public String sendEmail(@PathVariable("id") Long id, Model model, Principal principal) throws MessagingException {
+        Vacancy vacancy = vacancyRepository.findVacancyById(id);
+        User user = userService.findUserByEmail(principal.getName());
+        Employer employer = vacancy.getEmployer();
+        String employerEmail = employer.getUser().getEmail();
+        emailSenderService.sendEmail(employerEmail, user, vacancy);
+        return "emailsend";
     }
 }
